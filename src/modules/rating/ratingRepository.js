3be303ece4d18e92da671249531ceaa4d49ac5ed
aman <amangodara5686@gmail.com>
2026-08-16 16:12:22 +0530

    fixed the import error in ratingRepository file

--- src/modules/rating/ratingRepository.js
+++ src/modules/rating/ratingRepository.js
@@ -1,4 +1,4 @@
-import prisma from "../../config/prisma";
+import prisma from "../../config/prisma.js";
 
 const getUserRating = async (userId, type, options = {}) => {
   return await prisma.userRating.findUnique({
