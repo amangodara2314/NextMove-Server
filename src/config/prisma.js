fe53ced5cb247d31556afa79a14da093618c1ca4
aman <amangodara5686@gmail.com>
2026-09-10 15:40:49 +0530

    removed the duplicate generated folder

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from "../../prisma/generated/client.ts";
 dotenv.config();
 
 const adapter = new PrismaPg({
