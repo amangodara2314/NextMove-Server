739021922a722b25d5030b854753dcce1520c5dc
aman <amangodara5686@gmail.com>
2026-09-10 14:42:26 +0530

    fixing the prisma client import error

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
 dotenv.config();
 
 const adapter = new PrismaPg({
